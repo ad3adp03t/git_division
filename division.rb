4ea54b6bf373c8c1943fff38d9d43baaf5fbf3ba
ad3adp03t <ad3adp03t@gmail.com>
2017-06-23 22:36:24 -0400

    1st pass division

---
+++ division.rb
@@ -0,0 +1,22 @@
+def numb1
+	num1
+end
+def numb2
+	num2
+end
+def ask(req)
+	puts req
+end
+
+ask ("What is your first number")	
+	num1 = gets.to_i
+ask ("What is your second number?")
+	num2 = gets.to_i
+if num1 ==0 or num2 ==0 
+	p "I'm sorry Dave, I'm afraid I can't do that"
+else
+	p "Your quotient is #{num1/num2}"
+end	
+=begin Had to add if else statement 
+to prevent 0 from being used as dividend
+=end
