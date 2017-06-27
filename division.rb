64cafc994d38065b701ad0ddc6ac3bdc42a0c865
ad3adp03t <ad3adp03t@gmail.com>
2017-06-26 23:29:35 -0400

    1st draft divsion HW

--- division.rb
+++ division.rb
@@ -12,11 +12,13 @@ ask ("What is your first number")
 	num1 = gets.to_i
 ask ("What is your second number?")
 	num2 = gets.to_i
-if num1 ==0 or num2 ==0 
+if num2 ==0 
 	p "I'm sorry Dave, I'm afraid I can't do that"
 else
-	p "Your quotient is #{num1/num2}"
+	p "Your quotient is #{num1/num2.to_f}"
 end	
 =begin Had to add if else statement 
 to prevent 0 from being used as dividend
+.to_f forces floating point division instead of 
+integer division
 =end
--- marc.rb
+++
@@ -1,8 +0,0 @@
-def num1(x)
-	x
-end
-def num2(y)
-	y
-end	
-
-puts [num1(18)+num2(5)]
